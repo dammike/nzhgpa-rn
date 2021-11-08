b02fc4b91655b6a542d748d6416691df583afee9
dammike <dammikesaman@gmail.com>
2021-11-08 18:38:23 +1300

    update LoginScreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,15 +3,26 @@ import { StyleSheet, Text, TextInput, View } from 'react-native';
 import AppButton from '../components/AppButton';
 
 import AppTextInput from '../components/AppTextInput';
-import AppText from '../components/AppText';
 import Screen from '../components/Screen';
-import colors from '../config/colors';
+import IconComponent from '../components/IconComponent';
 
 export default function LoginScreen() {
     return (
         <Screen>
-            <AppTextInput placeholder="Email" />
-            <AppTextInput placeholder="Password" />
+            <AppTextInput
+                autoCorrect={false}
+                keyboardType={'email-address'}
+                maxLength={55}
+                placeholder="Email"
+                textContentType={'emailAddress'}
+                IconComponent={<IconComponent name="email" />} />
+            <AppTextInput
+                autoComplete={false}
+                autoCorrect={false}
+                maxLength={55}
+                placeholder="Password"
+                IconComponent={<IconComponent name="lock" />}
+                secureTextEntry />
             <AppButton title="Submit" onPress={() => console.log('submit')} />
         </Screen>
     )
