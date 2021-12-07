a0afd56fe4d50fa1dc4fe1f610ea38376a437cc5
Dammike Saman <dimmuborgir.damie@gmail.com>
2021-12-07 16:57:04 +1300

    Update airs api to post AIRS data.

--- app/api/airs.js
+++ app/api/airs.js
@@ -2,7 +2,9 @@ import client from './client';
 
 const endpoint = "/airs";
 const getAirs = () => client.get(endpoint);
+const postAirs = (record) => client.post(endpoint, record);
 
 export default {
-    getAirs
+    getAirs,
+    postAirs
 }
